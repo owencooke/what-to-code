3732170e81b771ee78bf5b715b474bf4a1745621
Zack Dorward <zdorward1@gmail.com>
2024-06-01 22:31:52 -0600

    potential bug fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
