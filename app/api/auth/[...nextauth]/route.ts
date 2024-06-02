2d7e70e2d2ef057a0ad7a1b131db7482ebbcdfbc
Zack Dorward <zdorward1@gmail.com>
2024-06-01 22:02:31 -0600

    added github auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
