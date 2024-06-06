244d4bcbe33e960dd4c48ed28b0845b5bc3de227
Owen Cooke <ocooke@ualberta.ca>
2024-06-05 20:58:46 -0400

    improve idea prompt

--- app/api/idea/logic.ts
+++ app/api/idea/logic.ts
@@ -7,8 +7,19 @@ import { PromptTemplate } from "@langchain/core/prompts";
 // TODO: add {list_of_idea_types} to the template
 const IDEA_PROMPT = `
     {format_instructions}
-    Generate a random idea for a software project.
-`;
+    You are a creative entrepreneur looking to generate a new, innovative product idea.
+    The product will be built using software, not a service or physical good. 
+    The ideas are just ideas. The product need not yet exist, nor may it necessarily be clearly feasible. 
+
+    An example of what the output should look like is provided below:
+
+    title: VR Medical Training
+    description: An immersive software platform designed to provide realistic scenarios for medical students and professionals to practice complex procedures and decision-making. Users will be able to enhance their skills and knowledge in a safe virtual environment. This innovative tool aims to revolutionize medical training by offering a cost-effective and accessible solution for hands-on learning.
+
+    It is your job as the creative entrepreneur to generate the unique software project idea.
+    It is CRITICAL that the description not discuss specific software or application features. 
+    It should only describe the high level benefits of the project for potential users.
+    `;
 
 const ideaParser = StructuredOutputParser.fromZodSchema(
   z.object({
