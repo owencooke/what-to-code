b09a047515ca8b6d03727ea7473ac42ce30e8b04
Owen Cooke <ocooke@ualberta.ca>
2024-07-06 21:31:45 -0400

    don't show error modal if already subscribed

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -7,12 +7,36 @@ export async function POST(req: NextRequest) {
     const email = req.nextUrl.searchParams.get("email");
     const { error } = await supabase.from("subscribers").insert([{ email }]);
     if (error) {
-      console.log(error);
+      if (error.code === "23505") {
+        return NextResponse.json({
+          message: `Email ${email} is already registered`,
+        });
+      }
       throw error;
     }
-
     return NextResponse.json({
       message: `Email subscription successful for ${email}`,
+    });
+  } catch (e: any) {
+    return NextResponse.json(
+      { error: e.message, details: e.details },
+      { status: e.status ?? 500 },
+    );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const email = req.nextUrl.searchParams.get("email");
+    const { error } = await supabase
+      .from("subscribers")
+      .delete()
+      .eq("email", email);
+    if (error) {
+      throw error;
+    }
+    return NextResponse.json({
+      message: `Email ${email} deleted successfully`,
     });
   } catch (e: any) {
     return NextResponse.json(
