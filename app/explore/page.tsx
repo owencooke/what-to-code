63b6a97dcc28188546eb13e9128160b867872a0f
Zack Dorward <zdorward1@gmail.com>
2024-07-08 15:27:03 -0600

    the queries work I believe

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -2,123 +2,93 @@
 
 import { useRouter } from "next/navigation";
 import { Card, CardHeader } from "@/components/ui/card";
-import { useState, useEffect, useMemo } from "react";
-import defaultIdea from "@/app/idea/data/defaultIdea";
-import { Button, ButtonWithLoading } from "@/components/ui/button";
-import FeatureCard from "@/components/cards/FeatureCard";
+import { useState, useEffect } from "react";
+import { Project } from "@/types/project";
+import { useSession } from "next-auth/react";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import FeatureCard from "@/components/cards/FeatureCard";
 import FrameworkCard from "@/components/cards/FrameworkCard";
-import { SkeletonCard } from "@/components/cards/SkeletonCard";
-import { Skeleton } from "@/components/ui/skeleton";
+import RepoDisplay from "@/components/github/Repo";
+import { getRepoFromTitle } from "../api/project/github";
 
 export default function Home() {
-  const router = useRouter();
+  const { data: session } = useSession();
 
-  const [idea, setIdea] = useState(defaultIdea);
-  const [isIdeaLoading, setIsIdeaLoading] = useState(false);
-  const [isInterested, setIsInterested] = useState(false);
+  const router = useRouter();
+  const [projects, setProjects] = useState<Project[]>([]);
 
   useEffect(() => {
-    setIsIdeaLoading(false);
-  }, [idea]);
-
-  const handleExpandIdea = async () => {
-    setIsInterested(true);
-    const response = await fetch(`/api/idea`, {
-      method: "POST",
-      body: JSON.stringify({
-        title: idea.title,
-        description: idea.description,
-      }),
-    });
-    if (!response.ok) {
-      console.error("Failed to expand on idea:", response.statusText);
-      return;
+    const redirect = () => router.push("/");
+    const fetchProject = async () => {
+      try {
+        const response = await fetch(`/api/user/feed`, {
+          method: "GET",
+          headers: {
+            Authorization: "token " + session?.accessToken,
+          },
+        });
+        if (!response.ok) {
+          redirect();
+          return;
+        }
+        const projectData = await response.json();
+        setProjects(projectData);
+      } catch (error) {
+        redirect();
+      }
+    };
+    if (session?.accessToken) {
+      fetchProject();
     }
-    const data = await response.json();
-    setIdea({ ...idea, ...data });
-  };
-
-  const handleCreateProject = async () => {
-    sessionStorage.setItem("idea", JSON.stringify(idea));
-    router.push("/project/create");
-  };
-
-  const skeletonCards = useMemo(
-    () => Array.from({ length: 3 }).map((_, i) => <SkeletonCard key={i} />),
-    [],
-  );
+  }, [session?.accessToken, router]);
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-7xl mt-12 mb-6">hmm, what to code?</h1>
-      <Card className="mt-8 w-4/5">
-        <CardHeader className="gap-8">
-          <>
-            {isIdeaLoading ? (
-              <>
-                <Skeleton className="h-12 w-3/4" />
-                <Skeleton className="h-28 w-full" />
-              </>
-            ) : (
-              <>
-                <h1>{idea.title}</h1>
-                <p>{idea.description}</p>
-                {!isInterested ? (
-                  <div className="flex justify-center">
-                    <ButtonWithLoading
-                      onClick={handleExpandIdea}
-                      loadingText="expanding your idea..."
-                    >
-                      {"i'm interested"}
-                    </ButtonWithLoading>
-                  </div>
-                ) : (
-                  <>
-                    <>
-                      <h2>what to build</h2>
-                      <ScrollArea className="mt-4">
-                        <div className="flex gap-12">
-                          {idea.features?.length > 0 ? (
-                            idea.features.map((feature, i) => (
-                              <FeatureCard key={i} feature={feature} />
-                            ))
-                          ) : (
-                            <>{skeletonCards}</>
-                          )}
-                        </div>
-                        <ScrollBar orientation="horizontal" />
-                      </ScrollArea>
-                    </>
-                    <>
-                      <h2>how to build it</h2>
-                      <ScrollArea className="mt-4">
-                        <div className="flex gap-12">
-                          {idea.frameworks?.length > 0 ? (
-                            idea.frameworks.map((framework, i) => (
-                              <FrameworkCard key={i} framework={framework} />
-                            ))
-                          ) : (
-                            <>{skeletonCards}</>
-                          )}
-                        </div>
-                        <ScrollBar orientation="horizontal" />
-                      </ScrollArea>
-                    </>
-                    <Button
-                      size="lg"
-                      disabled={!idea.features}
-                      onClick={handleCreateProject}
-                    >
-                      help me get started
-                    </Button>
-                  </>
-                )}
-              </>
-            )}
-          </>
-        </CardHeader>
-      </Card>
+      <div className="m-8 w-4/5">
+        {projects.length > 0 ? (
+          projects.map((project) => (
+            <div
+              className="flex flex-col lg:flex-row gap-8"
+              key={project.description}
+            >
+              <Card className="w-full lg:w-2/5">
+                <CardHeader>
+                  <h1 className="text-5xl my-4">{project.title}</h1>
+                  <h4>{project.description}</h4>
+                  {/* FIXME: repo display needs to display user from project, not necessarily current active user */}
+                  <RepoDisplay
+                    className="pt-4"
+                    name={getRepoFromTitle(project.title)}
+                    isClickable
+                  />
+                </CardHeader>
+              </Card>
+              <Card className="w-full h-full lg:w-3/5">
+                <CardHeader className="gap-4 p-4">
+                  <h2 className="text-xl">Features</h2>
+                  <ScrollArea>
+                    <div className="flex gap-4">
+                      {project.features?.map((feature, i) => (
+                        <FeatureCard key={i} feature={feature} />
+                      ))}
+                    </div>
+                    <ScrollBar orientation="horizontal" />
+                  </ScrollArea>
+                  <h2 className="text-xl mt-4">Type of Project</h2>
+                  <FrameworkCard framework={project.framework} />
+                </CardHeader>
+              </Card>
+            </div>
+          ))
+        ) : (
+          <Card className="mt-8 w-4/5">
+            <CardHeader className="gap-8">
+              <>No projects found</>
+            </CardHeader>
+          </Card>
+        )}
+      </div>
     </div>
   );
 }
