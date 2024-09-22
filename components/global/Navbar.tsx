503d10481e2b7f595999a948637f49ca070c42a1
Owen Cooke <ocooke@ualberta.ca>
2024-09-21 22:49:51 -0700

    muted color nav/footer

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -96,7 +96,7 @@ export const Navbar = () => {
   );
 
   return (
-    <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
+    <header className="sticky border-b-[1px] top-0 z-40 w-full bg-muted">
       <NavigationMenu className="mx-auto">
         <NavigationMenuList className="container h-14 px-4 w-[95vw] flex justify-between items-center">
           <NavigationMenuItem className="flex min-w-fit h-full !mt-0">
