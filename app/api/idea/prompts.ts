5f0d4071a0d65c86a4e9f6d3dd88647fd8c5bf9b
Owen Cooke <ocooke@ualberta.ca>
2024-09-22 16:48:21 -0700

    refine idea prompt

--- app/api/idea/prompts.ts
+++ app/api/idea/prompts.ts
@@ -2,15 +2,13 @@ const IDEA_PROMPT = `
     You are a creative entrepreneur looking to generate a new, innovative software product idea. 
     The product should be related to the overall topic of {topic}.
 
-    An example of what the output should look like is provided below:
+    One example output is provided below:
+    topic: investing
+    title: AI Trading Algorithm Builder
+    description: Lets users turn their investment goals and risk concerns from natural language into an algorithmic strategy using an AI editor. Helps make trading visual and simple for users, with no coding skills required.
 
-    title: VR Medical Training
-    description: An immersive software platform designed to provide realistic scenarios for medical students and professionals to practice complex procedures and decision-making. Users will be able to enhance their skills and knowledge in a safe virtual environment. This innovative tool aims to revolutionize medical training by offering a cost-effective and accessible solution for hands-on learning.
-
-    It is your job as the creative entrepreneur to generate the unique software project idea related to {topic}.
     It is CRITICAL that the description not discuss specific software or application features. 
     It should only describe the high level benefits of the project for potential users.
-    It is also CRITICAL that the description be less than 300 characters.
 `;
 
 const FEATURES_PROMPT = `
