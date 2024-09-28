b4ca86947365cac2ee8c804d31bca13da62c0519
Owen Cooke <ocooke@ualberta.ca>
2024-09-28 02:25:18 -0700

    update states

--- app/project/create/match-repos.tsx
+++ app/project/create/match-repos.tsx
@@ -11,10 +11,17 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { StarIcon, GitForkIcon, GithubIcon } from "lucide-react";
+import {
+  StarIcon,
+  GitForkIcon,
+  GithubIcon,
+  SearchIcon,
+  RocketIcon,
+} from "lucide-react";
 import { useSession } from "next-auth/react";
 import { GitHubRepo } from "@/types/github";
 import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function MatchedRepos({
   techDescription,
@@ -46,22 +53,25 @@ export default function MatchedRepos({
 
   if (isLoading) {
     return (
-      <div className="flex justify-center items-center h-64">
-        Loading repositories...
+      <div className="flex justify-center items-center h-48">
+        <RocketIcon className="w-12 h-12 animate-bounce text-primary" />
+        <span className="ml-4">Searching for matches...</span>
       </div>
     );
   }
 
   if (error) {
     return (
-      <div className="text-red-500 text-center">
-        Failed to fetch repositories: {error.message}
+      <div className="text-red-500 flex justify-center items-center h-48">
+        {`Failed to fetch repositories. Please try again later :(`}
       </div>
     );
   }
 
   if (!repos || repos.length === 0) {
-    return <div className="text-center">No matching template repositories</div>;
+    return (
+      <div className="flex justify-center items-center h-48">{`Sorry, we couldn't find any matching repositories :(`}</div>
+    );
   }
 
   return (
@@ -69,16 +79,7 @@ export default function MatchedRepos({
       {repos.map((repo) => (
         <Card key={repo.url} className="flex flex-col">
           <CardHeader>
-            <CardTitle className="text-lg">
-              <Link
-                href={repo.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="transition-colors hover:text-secondary"
-              >
-                <GithubIcon className="inline" /> {repo.name}
-              </Link>
-            </CardTitle>
+            <CardTitle className="text-lg">{repo.name}</CardTitle>
             <CardDescription className="line-clamp-2">
               {repo.description}
             </CardDescription>
@@ -94,6 +95,14 @@ export default function MatchedRepos({
           </CardContent>
           <CardFooter className="flex flex-col items-start gap-2">
             <div className="flex items-center gap-4">
+              <Link
+                href={repo.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${buttonVariants({ variant: "link" })} !p-0`}
+              >
+                <GithubIcon className="inline" />
+              </Link>
               <span className="flex items-center gap-1">
                 <StarIcon className="w-4 h-4" />
                 {repo.stars}
