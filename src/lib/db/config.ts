3e5f5e762d2faf937da0e012d8e57507d1a12ced
Owen Cooke <ocooke@ualberta.ca>
2024-11-24 19:18:38 -0800

    stop using pooler host url

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -1,9 +1,8 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 // Construct the connection string
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 // Create the drizzle database instance with the SQL client
-export const db = drizzle(neon(connectionString));
+export const db = drizzle(connectionString);
