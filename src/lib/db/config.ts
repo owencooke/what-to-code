9da18cc076253ad2c8eb688d0e07a16b7f55af46
Owen Cooke <ocooke@ualberta.ca>
2024-11-24 22:48:37 -0800

    add timeout for cold start

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/neon-serverless";
 
 // Construct the connection string
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&connection_limit=1`;
+export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&pgbouncer=true&connect_timeout=10&pool_timeout=10`;
 
 export const db = drizzle(connectionString);
