8c41fd1ebbd95d0cdc2c1ca9a923ea9e268544a5
Owen Cooke <ocooke@ualberta.ca>
2024-11-24 22:39:18 -0800

    1 connection limit

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -1,10 +1,7 @@
-import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
 // Construct the connection string
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`;
+export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&connection_limit=1`;
 
-// Create the drizzle database instance with the SQL client
-const pool = new Pool({ connectionString });
-export const db = drizzle(pool);
+export const db = drizzle(connectionString);
