fa1dfa0e9039795db30fef4690f2942ca286e159
Owen Cooke <ocooke@ualberta.ca>
2024-11-24 20:23:57 -0800

    try using connection pool

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -1,8 +1,10 @@
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
 // Construct the connection string
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require`;
 
 // Create the drizzle database instance with the SQL client
-export const db = drizzle(connectionString);
+const pool = new Pool({ connectionString });
+export const db = drizzle(pool);
