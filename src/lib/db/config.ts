8584a8849df8ccdde7ce618b12cfb3ebb79f65f9
Owen Cooke <ocooke@ualberta.ca>
2024-11-24 18:43:18 -0800

    try neon http

--- src/lib/db/config.ts
+++ src/lib/db/config.ts
@@ -1,8 +1,9 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 // Construct the connection string
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 export const connectionString = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 // Create the drizzle database instance with the SQL client
-export const db = drizzle(connectionString);
+export const db = drizzle(neon(connectionString));
